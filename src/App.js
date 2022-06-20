0b7f115100c40ecae9d5bf37990b879cd30f9840
RAJDEEP KUMAR GAUTAM <rajdeepkgautam@gmail.com>
2022-06-20 14:55:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EventHandling from './components/eventhandling';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>EMPLOYEE FEEDBACK FORM</h1>
+      <EventHandling/>
+    </div>
+  );
+}
+
+export default App;
